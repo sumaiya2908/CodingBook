6a7b9f70f805fce7755a16d1bb1b8bd10c5864fe
unknown <sumaiyahas2908@gmail.com>
2022-09-07 13:30:20 +0530

    feat: add Node class

---
+++ BFSforTree.java
@@ -0,0 +1,15 @@
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    public TreeNode(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+public class BFSforTree {
+    public static void main(String args[]) {
+        System.out.print("hrllo");
+    }
+}
